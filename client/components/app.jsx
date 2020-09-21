fe579f5fb831374b57a10b82f2197fad39167cfd
swarnalats <swarna_latha@hotmail.com>
2020-09-21 16:08:05 -0700

    Completed feature sets 1-6

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      cart: [],
       message: null,
       isLoading: true,
       view: {
@@ -15,6 +16,30 @@ class App extends React.Component {
       }
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  getCartItems() {
+    fetch('/api/cart')
+      .then(res => res.json())
+      .then(data => {
+        console.log('data from fetch', data);
+        this.setState({ cart: data.length });
+      })
+      .catch(err => this.setState({ message: err.message }));
+  }
+
+  addToCart(product) {
+    fetch('/api/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+      .then(data => { this.setState({ cart: this.state.cart + 1 }); console.log('No', this.state.cart); })
+      .catch(error => { console.error('Error:', error); });
   }
 
   setView(name, params) {
@@ -34,18 +59,19 @@ class App extends React.Component {
       .then(data => this.setState({ message: data.message || data.error }))
       .catch(err => this.setState({ message: err.message }))
       .finally(() => this.setState({ isLoading: false }));
+    this.getCartItems();
   }
 
   render() {
     return !this.state.isLoading
       ? this.state.view.name === 'catalog'
-        ? <div>
-          <Header title="Wicked Sales" />
+        ? <div >
+          <Header title="$ Wicked Sales" cartCount={this.state.cart}/>
           <ProductList view={this.setView} />
         </div>
-        : <div>
-          <Header title="Wicked Sales" />
-          <ProductDetails viewState = {this.state.view.params} viewMethod={this.setView}/>
+        : <div >
+          <Header title="$ Wicked Sales" cartCount={this.state.cart}/>
+          <ProductDetails viewState = {this.state.view.params} viewMethod={this.setView} addToCart={this.addToCart}/>
         </div>
       : <h1>{ this.state.message }</h1>;
   }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 function Header(props) {
-  return <h1>{props.title}</h1>;
+  return (
+    <div className={'container back'}>
+      <div className={'headerStyle row'}>
+        <h4 className={'col-md-6'}>{props.title}</h4>
+        <h4 className={'col-md-6'} style={{ textAlign: 'right' }}>
+          <span ><i style={{ textAlign: 'right' }} className={'fas fa-shopping-cart'}></i> </span>
+          <span>{props.cartCount} Items </span>
+        </h4>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
